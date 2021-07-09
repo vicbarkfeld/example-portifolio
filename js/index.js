4e22c518db6d1aef0988a9a36e30cec237d55067
Victoria Barkfeld <victoriacbarkfeld@gmail.com>
2021-07-09 20:51:51 -0300

    apply js in the services

--- js/index.js
+++ js/index.js
@@ -29,6 +29,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header')
@@ -69,7 +70,27 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+       modalViews.forEach((modalView) =>{
+           modalView.classList.remove('active-modal')
+       })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
